d1c7a666ae628f7b60c1df821bb3fc163cdc04eb
wangkun <wangkun@meitunmama.com>
2018-07-30 18:26:34 +0800

    增加本地存储模块

---
+++ src/utils/localStorage.js
@@ -0,0 +1,52 @@
+/**
+ * localStorage 封装类，除了提供通用的get/set/remove之外，还提供一个过期时间的选项
+ * 类似cookie的使用方式
+ * 
+ * timeout默认接受可被new Date()识别的字符串或时间戳两种格式
+ */
+import _ from 'lodash'
+
+export default {
+  get(key) {
+    if(!key) return null
+
+    let target = localStorage.getItem(key)
+    if(_.isObject(target) && _.isNumber(target.timeout)){
+      let now = new Date().getTime()
+
+      if(now >= target.timeout){
+        return target.value
+      } else {
+        localStorage.removeItem(key)
+      }
+    } else {
+      localStorage.removeItem(key)
+    }
+  },
+  set(key, value, timeout) {
+    if(!key) return
+
+    if(timeout !== undefined){
+      if(_.isString(timeout)){
+        timeout = new Date(timeout)
+        timeout = _.isDate(timeout) ? timeout.getTime() : -1
+      }else if(!_.isNumber(timeout)){
+        timeout = -1
+      }
+    }
+    
+    if(value !== undefined){
+      let target = {
+        val: value,
+        timeout: timeout,
+      }
+
+      localStorage.setItem(key, target)
+    }
+  },
+
+  remove(key) {
+    if(!key) return
+    localStorage.removeItem(key)
+  },
+}
